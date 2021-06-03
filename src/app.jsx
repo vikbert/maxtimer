4f51243c61d3ee3b817dbbd83b865f35f188a501
vikbert <segentor@gmail.com>
2021-06-03 07:39:00 +0200

    updates

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,22 @@
 import {render} from 'preact';
+import {useEffect} from 'preact/hooks';
 import SlotList from './components/slot/SlotList';
+import {useState} from 'preact/compat';
 
+const AUTO_REFRESH_minutes = 3;
 function App() {
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setTime(Date.now()),
+      AUTO_REFRESH_minutes * 60 * 1000,
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return <SlotList />;
 }
 
