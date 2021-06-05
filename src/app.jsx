4c1b1e9c4c9f1e0a8169af5887ab3ce377099985
vikbert <segentor@gmail.com>
2021-06-05 21:26:14 +0200

    updates

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,19 @@ import {useState} from 'preact/compat';
 import SlotListContainer from './components/slot/SlotListContainer';
 import 'native-toast/dist/native-toast.css';
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://rfvuywxobqfyspzstpya.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+let { data, error } = await supabase
+  .from('record')
+  .select('*')
+
+  console.log('fetched data: ', data)
+
+
 const AUTO_REFRESH_minutes = 3;
 function App() {
   const [time, setTime] = useState(Date.now());
